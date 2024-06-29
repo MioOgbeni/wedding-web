c22872be046c85d7e7026d6f7f321831f0f03727
Tomáš Novák <tom.nov96@gmail.com>
2024-06-29 23:08:25 +0200

    chore: Update cat icons size in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,14 @@ import heartIcon from '../assets/images/heart.svg';
 function Footer() {
   return (
     <footer>
-        <p className="author">Lucie & Tomáš</p>
-        <p className="author">24–8–24</p>
         <img src={heartIcon} alt="Srdce ikona" className="cat-icon" />
         <div className="cat-container">
           <img src={oggyIcon} alt="Oggy ikona" className="cat-icon" />
           <img src={tomIcon} alt="Tom ikona" className="cat-icon" />
           <img src={viaIcon} alt="Via ikona" className="cat-icon" />
         </div>
+        <p className="author">Lucie & Tomáš</p>
+        <p className="author">24–8–24</p>
     </footer>
   );
 }
