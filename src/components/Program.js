de64b5b11df05d617eb11b256b6e289e00f688f2
Tomáš Novák <tom.nov96@gmail.com>
2024-07-08 19:55:12 +0200

    chore: Update contact details and titles in Contacts and Program components

--- src/components/Program.js
+++ src/components/Program.js
@@ -10,37 +10,37 @@ import heartIcon from '../assets/images/srdicko.svg';
 function Program() {
   return (
     <div className="details-container">
-      <div className="details-point-program">
-        <img src={arriveIcon} alt="Příjezd ikona" className="details-icon-program" />
-        <h2 className="details-title-program">13.00–14.00</h2>
+      <div className="details-point">
+        <img src={arriveIcon} alt="Příjezd ikona" className="details-icon" />
+        <h2 className="details-title">13.00–14.00</h2>
         <p className="details-description">
            příjezd hostů
         </p>
       </div>
-      <div className="details-point-program">
-        <img src={heartIcon} alt="Obřad ikona" className="details-icon-program" />
-        <h2 className="details-title-program">14.30</h2>
+      <div className="details-point">
+        <img src={heartIcon} alt="Obřad ikona" className="details-icon" />
+        <h2 className="details-title">14.30</h2>
         <p className="details-description">
           OBŘAD
         </p>
       </div>
-      <div className="details-point-program">
-        <img src={cheersIcon} alt="Přípitek ikona" className="details-icon-program" />
-        <h2 className="details-title-program">15.00</h2>
+      <div className="details-point">
+        <img src={cheersIcon} alt="Přípitek ikona" className="details-icon" />
+        <h2 className="details-title">15.00</h2>
         <p className="details-description">
           přípitek a krájení dortu
         </p>
       </div>
-      <div className="details-point-program">
-        <img src={grillIcon} alt="Grilovačka ikona" className="details-icon-program" />
-        <h2 className="details-title-program">15.30</h2>
+      <div className="details-point">
+        <img src={grillIcon} alt="Grilovačka ikona" className="details-icon" />
+        <h2 className="details-title">15.30</h2>
         <p className="details-description">
           grilovačka a volná zábava
         </p>
       </div>
-      <div className="details-point-program">
-        <img src={leaveIcon} alt="Odjezd ikona" className="details-icon-program" />
-        <h2 className="details-title-program">22.00</h2>
+      <div className="details-point">
+        <img src={leaveIcon} alt="Odjezd ikona" className="details-icon" />
+        <h2 className="details-title">22.00</h2>
         <p className="details-description">
           odjezd
         </p>
